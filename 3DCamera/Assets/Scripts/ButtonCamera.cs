384cdcce8350d6932e6113bb0d00c3c03981ef02
DESKTOP-RTS1VK8\TranNam <trantunam.cqtk8@gmail.com>
2016-12-06 22:04:40 +0700

    create

---
+++ 3DCamera/Assets/Scripts/ButtonCamera.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ButtonCamera : MonoBehaviour {
+
+    public Button btnCamera;
+    public bool changeCamera = false;
+
+    // Use this for initialization
+    void Start () {
+        Button btn = btnCamera.GetComponent<Button>();
+        btn.onClick.AddListener(Click);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Click() {
+        changeCamera = !changeCamera;
+    }
+}
