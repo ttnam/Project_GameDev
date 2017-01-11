60b8b4a89e5f70a1549a79a7685e64b7a422abaf
TranNam <trantunam.cqtk8@gmail.com>
2017-01-11 22:40:24 +0700

    add Pisa at Italia

---
+++ Assets/Marvel/SpinObject.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpinObject : MonoBehaviour {
+    public GameObject earth;
+    
+
+    private void Start()
+    {
+        transform.SetParent(earth.transform);
+    }
+
+    private void Update()
+    {
+        
+    }
+}
