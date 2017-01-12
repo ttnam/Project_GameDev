88c63863a8c0ae8bac609ef6aead355e24a5037c
Phuc-Henry <nphau.itus@gmail.com>
2017-01-12 14:45:59 +0700

    [f] update

---
+++ Assets/Scripts/DialogPanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DialogPanel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void go()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+    public void option()
+    {
+        SceneManager.LoadScene("OptionScene");
+
+    }
+    public void contact()
+    {
+        Application.OpenURL("https://www.google.com");
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Models/Wonder.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Wonder
+{
+
+    private string id;
+    private string name;
+    private string nation;
+    private int[] view = new int[3];
+    private List<string> images;
+
+
+    public Wonder()
+    {
+        this.id = "";
+        this.name = "";
+        this.nation = "";
+        this.view[0] = 0;
+        this.view[1] = 0;
+        this.view[2] = 0;
+        this.images = new List<string>();
+    }
+
+}
