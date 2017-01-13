58f20682fefeb8d947274c551337b5fef5a4d49f
Phuc-Henry <nphau.itus@gmail.com>
2017-01-13 13:25:48 +0700

    [r]  refactor

---
+++ Assets/Scripts/DetailScene.cs
@@ -0,0 +1,176 @@
+﻿#region Namespaces
+
+using UnityEngine;
+using System.Collections;
+
+#endregion // Namespaces
+
+public class DetailScene : MonoBehaviour
+{
+
+	#region Variables
+
+	// Canvas
+	public Canvas m_Canvas;
+	
+	// GUIAnimFREE object of dialogs
+	public GUIAnimFREE m_Dialog;
+    public GUIAnimFREE m_Dialog_Info;
+    public GUIAnimFREE m_DialogButtons;
+	
+	#endregion // Variables
+	
+	
+	#region MonoBehaviour
+	
+	// Awake is called when the script instance is being loaded.
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.Awake.html
+	void Awake ()
+	{
+		if(enabled)
+		{
+			// Set GUIAnimSystemFREE.Instance.m_AutoAnimation to false in Awake() will let you control all GUI Animator elements in the scene via scripts.
+			GUIAnimSystemFREE.Instance.m_AutoAnimation = false;
+		}
+	}
+	
+	void Start ()
+	{
+		
+		StartCoroutine(MoveInTitleGameObjects());
+
+		// Disable all scene switch buttons
+		// http://docs.unity3d.com/Manual/script-GraphicRaycaster.html
+		GUIAnimSystemFREE.Instance.SetGraphicRaycasterEnable(m_Canvas, false);
+	}
+	
+	// Update is called every frame, if the MonoBehaviour is enabled.
+	// http://docs.unity3d.com/ScriptReference/MonoBehaviour.Update.html
+	void Update ()
+	{		
+	}
+	
+	#endregion // MonoBehaviour
+	
+	// ########################################
+	// MoveIn/MoveOut functions
+	// ########################################
+	
+	#region MoveIn/MoveOut
+	
+	// MoveIn m_Title1 and m_Title2
+	IEnumerator MoveInTitleGameObjects()
+	{
+		yield return new WaitForSeconds(1.0f);
+		
+		// MoveIn all dialogs and buttons
+		StartCoroutine(MoveInPrimaryButtons());
+	}
+	
+	// MoveIn all dialogs and buttons
+	IEnumerator MoveInPrimaryButtons()
+	{
+		yield return new WaitForSeconds(1.0f);
+		
+		// MoveIn all dialogs
+		m_Dialog.MoveIn(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_Dialog_Info.MoveIn(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_DialogButtons.MoveIn(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+				
+		// Enable all scene switch buttons
+		StartCoroutine(EnableAllDemoButtons());
+	}
+	
+	// MoveOut all dialogs and buttons
+	public void HideAllGUIs()
+	{
+		// MoveOut all dialogs
+		m_Dialog.MoveOut(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_Dialog_Info.MoveOut(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_DialogButtons.MoveOut(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+		
+		
+		// MoveOut m_Title1 and m_Title2
+		StartCoroutine(HideTitleTextMeshes());
+	}
+	
+	// MoveOut m_Title1 and m_Title2
+	IEnumerator HideTitleTextMeshes()
+	{
+		yield return new WaitForSeconds(1.0f);
+		
+	}
+	
+	#endregion // MoveIn/MoveOut
+	
+	// ########################################
+	// Enable/Disable button functions
+	// ########################################
+	
+	#region Enable/Disable buttons
+	
+	// Enable/Disable all scene switch Coroutine
+	IEnumerator EnableAllDemoButtons()
+	{
+		yield return new WaitForSeconds(1.0f);
+
+		// Enable all scene switch buttons
+		// http://docs.unity3d.com/Manual/script-GraphicRaycaster.html
+		GUIAnimSystemFREE.Instance.SetGraphicRaycasterEnable(m_Canvas, true);
+	}
+
+	// Disable all buttons for a few seconds
+	IEnumerator DisableButtonForSeconds(GameObject GO, float DisableTime)
+	{
+		// Disable all buttons
+		GUIAnimSystemFREE.Instance.EnableButton(GO.transform, false);
+		
+		yield return new WaitForSeconds(DisableTime);
+		
+		// Enable all buttons
+		GUIAnimSystemFREE.Instance.EnableButton(GO.transform, true);
+	}
+	
+	#endregion // Enable/Disable buttons
+	
+	// ########################################
+	// UI Responder functions
+	// ########################################
+	
+	#region UI Responder
+
+	public void OnDialogButton()
+	{
+		// MoveOut m_Dialog
+		m_Dialog.MoveOut(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_Dialog_Info.MoveOut(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_DialogButtons.MoveOut(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+		
+		// Disable m_DialogButtons for a few seconds
+		StartCoroutine(DisableButtonForSeconds(m_DialogButtons.gameObject, 2.0f));
+
+		// Moves m_Dialog back to screen
+		StartCoroutine(DialogMoveIn());
+	}
+	
+	#endregion // UI Responder
+	
+	// ########################################
+	// Move Dialog/Button functions
+	// ########################################
+	
+	#region Move Dialog/Button
+	
+	// Moves m_Dialog back to screen
+	IEnumerator DialogMoveIn()
+	{
+		yield return new WaitForSeconds(1.5f);
+		
+		m_Dialog.MoveIn(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_Dialog_Info.MoveIn(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+        m_DialogButtons.MoveIn(GUIAnimSystemFREE.eGUIMove.SelfAndChildren);
+	}
+	
+	#endregion // Move Dialog/Button
+    
+}
---
+++ Assets/Scripts/Helpers/Singleton.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+//
+// Summary:
+//     ///
+//     MONO BEHAVIER SINGLETON
+//     ///
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T mInstance = null;
+    public static T Instance
+    {
+        get
+        {
+            if (mInstance == null)
+            {
+                mInstance = (T)FindObjectOfType(typeof(T));
+                if (mInstance == null)
+                {
+                    string goName = typeof(T).ToString();
+                    GameObject go = GameObject.Find(goName);
+                    if (go == null)
+                    {
+                        go = new GameObject();
+                        go.name = goName;
+                    }
+                    mInstance = go.AddComponent<T>();
+                }
+            }
+            return mInstance;
+        }
+    }
+
+    //
+    // Summary:
+    //     ///
+    //     Garbage Collection
+    //     ///
+    public virtual void ApplicationQuit()
+    {
+        mInstance = null;
+    }
+    //
+    // Summary:
+    //     ///
+    //     parent this to another gameobject by string call from Awake if you so desire
+    //     ///
+    protected void SetParent(string parentGoName)
+    {
+        if (!string.IsNullOrEmpty(parentGoName))
+        {
+            GameObject parentGO = GameObject.Find(parentGoName);
+            if (parentGO == null)
+            {
+                parentGO = new GameObject();
+                parentGO.name = parentGoName;
+            }
+            this.transform.parent = parentGO.transform;
+        }
+    }
+}
